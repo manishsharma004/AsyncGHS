75d40b008478f450f5e94129a1120f38baac9b68
Siddhant Sahu <sunny1543@gmail.com>
2018-10-04 11:33:31 -0500

    - Updated Master Thread to include methods that it must implement and how it works with worker threads. - Updated Process so that it only has local information, i.e. knows only about its neighbours. Marked TODOs in code.

--- src/MasterThread.java
+++ src/MasterThread.java
@@ -1,32 +1,94 @@
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingDeque;
 
-public class MasterThread {
+public class MasterThread extends Thread {
+    /**
+     * The master thread is responsible for spawning and synchronizing the worker threads.
+     */
+    int id;
+    BlockingQueue<Message> queue = new LinkedBlockingDeque<>();
+    Process[] workers;
+    int numWorkers = 0;
+    HashSet<Integer> terminatedThreads = new HashSet<Integer>();
+    int numOfTerminatedThreads = 0;
+    Map<Integer, List<Process>> graph;  // TODO: make a new class for undirected graph and use that
 
-    public static void main(String args[]) {
 
-        int numWorkers = 4;
+    public MasterThread(String name, int id, Map<Integer, List<Process>> graph) {
+        super(name);
+        this.id = id;
+        this.graph = graph;
+    }
+
+    @Override
+    public synchronized void start() {
+        super.start();
+    }
+
+    private synchronized boolean pushToQueue(Process p, Message m) {
+        return p.queue.add(m);
+    }
+
+    synchronized public boolean broadcastMessage(Message msg) {
+        for (int i = 0; i < this.numWorkers; i++) {
+            pushToQueue(this.workers[i], msg);
+        }
+        return true;
+    }
+
+    synchronized public void handleMessage() throws InterruptedException {
+        /**
+         * Handles message received from workers. The workers can either terminate or choose to continue to the next
+         * round. If a worker thread sends a terminate signal to the master, it is terminated and the resources are
+         * freed. If it chooses to continue, the master thread awaits messages from all non-terminated threads to give a
+         * go-ahead for the next round. When the master receives a terminate signal from all workers, it shuts down.
+         */
+        // TODO: handle the above logic
+        Message out = queue.take();
+        if (out.message == -99 && !terminatedThreads.contains(out.sender)) {
+            this.terminatedThreads.add(out.sender);
+            this.numOfTerminatedThreads += 1;
+        }
+    }
+
+    public void spawnWorkers() {
+        int numWorkers = this.graph.keySet().size();
         Process[] workers = new Process[numWorkers];
-        for (int i = 0; i < workers.length; i++) {
-            workers[i] = new Process(("thread-" + i), i);
+
+        // assign neighbors by reading the graph
+        Set<Integer> keySet = this.graph.keySet();
+        Iterator<Integer> keySetIterator = keySet.iterator();
+        int i = 0;
+        while (keySetIterator.hasNext()) {
+            Integer vertexId = keySetIterator.next();
+            workers[i] = new Process("thread-" + vertexId, vertexId, this.graph.get(vertexId));
+            i += 1;
         }
 
-        // TODO: read a text file to load the graph and assign neighbours
-        // below is a sample graph of diameter 2
-        Map neighbors = new HashMap();
-        neighbors.put(0, Arrays.asList(workers[1], workers[2], workers[3]));
-        neighbors.put(1, Arrays.asList(workers[0]));
-        neighbors.put(2, Arrays.asList(workers[0], workers[3]));
-        neighbors.put(3, Arrays.asList(workers[2], workers[0]));
-
-        for (int i = 0; i < workers.length; i++) {
-            workers[i].setNeighbors(neighbors);
-            workers[i].setDiameter(2);
-        }
-
-        for (int i = 0; i < workers.length; i++) {
+        for (i = 0; i < workers.length; i++) {
             workers[i].start();
+        }
+
+        this.workers = workers;
+        this.numWorkers = numWorkers;
+        this.broadcastMessage(new Message(0, 0, -1));
+    }
+
+    @Override
+    public void run() {
+        try {
+            spawnWorkers();
+            System.out.println("Workers spawned");
+            while (true) {
+                if (this.numWorkers <= this.numOfTerminatedThreads) {
+                    System.out.println("All workers terminated. Terminating master now...");
+                    break;
+                }
+
+                handleMessage();
+            }
+        } catch (InterruptedException e) {
         }
     }
 }
